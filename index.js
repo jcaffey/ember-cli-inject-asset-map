84bd10fb643b54f198ed6128d212d2b360256d7b
Josh Caffey <jcaffey@gmail.com>
2015-07-23 22:54:10 -0500

    Inject dist

--- index.js
+++ index.js
@@ -16,9 +16,14 @@ module.exports = {
     // TODO: I'd love a cleaner way to do this, but I'm not sure sure how since this has to be done after build.
     var js          = fs.readFileSync(jsPath, 'utf-8'),
         assetMapKey = 'assetMapHash',
-        hackedJs    = js.replace(new RegExp(assetMapKey + ': undefined'), assetMapKey + ': ' + JSON.stringify(assetMap));
+        hackedJs    = js.replace(new RegExp(assetMapKey + ': undefined'), assetMapKey + ': ' + JSON.stringify(assetMap)),
+        hackedCompressed = js.replace(new RegExp(assetMapKey + ':void 0'), assetMapKey + ':' + JSON.stringify(assetMap));
 
+    // Inject in temp
     fs.writeFileSync(jsPath, hackedJs, 'utf-8');
+
+    // Inject in dist (this assumes dist is using JS compression.)
+    fs.writeFileSync(path.join('./dist', assetMap['assets/art19.js']), hackedCompressed, 'utf-8');
 
     console.log('Done! Asset paths are available in all components, controllers, and routes via assetMap.assetMapHash.');
   }
