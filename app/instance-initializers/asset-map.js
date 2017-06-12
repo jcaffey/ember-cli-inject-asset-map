1e28eb72279fd6d80fec760a337a08548247c5ca
SiuKit Wong <skwongr@gmail.com>
2017-06-12 15:39:16 -0500

    Use instance-initializers to support ember 2.x

--- app/instance-initializers/asset-map.js
+++ app/instance-initializers/asset-map.js
@@ -1,5 +1,5 @@
-export function initialize(container, app) {
-  app.register('assets:asset-map', container.lookup('controller:asset-map'), { instantiate: false, singleton: true });
+export function initialize(app) {
+  app.register('assets:asset-map', app.lookup('controller:asset-map'), { instantiate: false, singleton: true });
 
   app.inject('route', 'assetMap', 'assets:asset-map');
   app.inject('controller', 'assetMap', 'assets:asset-map');
